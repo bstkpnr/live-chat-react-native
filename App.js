d662516cb753786ac11cd15301ea6d00461f38cd
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2022-12-28 10:38:27 +0300

    added chatroom

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginPage from './pages/Login/LoginPage';
 import ChatPage from './pages/Chat/ChatPage';
 import Register from './pages/Register/Register';
+import ChatRoom from './pages/Chat/ChatRoom';
 
 function App() {
   const Stack = createNativeStackNavigator();
@@ -14,6 +15,7 @@ function App() {
         <Stack.Screen name="Login" component={LoginPage} />
         <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="Chat" component={ChatPage} />
+        <Stack.Screen name="ChatRoom" component={ChatRoom} />
       </Stack.Navigator>
     </NavigationContainer>
   );
