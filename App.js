3950af399633218488434474c5a6b99e187bb833
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2022-12-28 22:56:22 +0300

    delete chat page

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import LoginPage from './pages/Login/LoginPage';
-import ChatPage from './pages/Chat/ChatPage';
 import RegisterPage from './pages/Register/RegisterPage';
 import ChatRoom from './pages/Chat/ChatRoom';
 import {main_style} from './components/style/component_styles';
@@ -39,7 +38,6 @@ function App() {
           options={{headerShown: false}}
         />
         <Stack.Screen name="Login" component={LoginPage} options={{headerShown: false}} />
-        <Stack.Screen name="Chat" component={ChatPage} options={{headerShown: false}} />
         <Stack.Screen name="Message" component={ChatRoom} options={{headerShown: false}} />
         <Stack.Screen name="Register" component={RegisterPage} options={{headerShown: false}} />
         <Stack.Screen name="ChatRoom" component={ChatRoom} options={{headerShown: false}} />
