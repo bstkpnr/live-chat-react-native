1539a2fc84cb9f0b637b19d8f0d48338e9198cce
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2022-12-28 17:39:25 +0300

    edit button component

--- components/Button.js
+++ components/Button.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { TouchableOpacity,Text } from 'react-native'
 import { register_style } from './style/component_styles'
 
-export default function Button({title}) {
+export default function Button({title,onPress,style}) {
   return (
-    <TouchableOpacity style={register_style.container_x}>
-    <Text style={register_style.btn_title}>{title}</Text>
+    <TouchableOpacity style={style} onPress={onPress}>
+    <Text style={style}>{title}</Text>
 </TouchableOpacity>
   )
 }
